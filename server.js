f97569be39a462d53cfe76698ae783e2cc802ed8
Elten <elten@example.com>
2026-02-11 12:15:33 +0400

    Backend ve Frontend linkleri duzeldildi

--- server.js
+++ server.js
@@ -44,12 +44,8 @@ app.get('/api/team/:id/players', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log(`Server: http://localhost:3000`));
-// Əvvəlki kodun davamı...
-
-// Portu Render-in verdiyi dəyişəndən götürürük
+// DOĞRU HİSSƏ: Portu Render təyin edir
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server ${PORT} portunda aktivdir`);
 });
